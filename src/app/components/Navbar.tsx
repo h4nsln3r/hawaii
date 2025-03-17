13878852519f2f460b7b37b04efa433b1b023dd7
h4nsln3r <68617162+h4nsln3r@users.noreply.github.com>
2025-03-17 20:43:26 +0100

    Added first try logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,18 @@
-// components/Navbar.tsx
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
-          <h1 className="text-xl font-bold">Hawaii Shop</h1>
+          <Image 
+            src="/hawaii_logo.png"  // Bilden måste ligga i public/
+            alt="Företagslogga"
+            width={150} 
+            height={50}
+            priority // Optimerar laddning för navbaren
+          />
         </Link>
         <div>
           <Link href="/products" className="px-4">Produkter</Link>
